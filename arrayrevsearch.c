fbc414840f197469cf6b1301fe3f0aa7da8d1e66
M2yank <theghostmayank@gmail.com>
2025-01-29 10:18:30 +0530

    Add files via upload

---
+++ arrayrevsearch.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int arr[6] = {5, 3, 7, 1, 9, 8};
+    int target;
+    int found = -1;
+
+    printf("Enter the element to search: ");
+    scanf("%d", &target);
+
+    for (int i = 5; i >= 0; i--){
+        if (arr[i] == target) {
+            found = i;
+            break;
+        }
+    }
+
+    if (found != -1) {
+        printf("Element found at index %d\n", found);
+    } else {
+        printf("Element not found in the array\n");
+    }
+
+    return 0;
+}
